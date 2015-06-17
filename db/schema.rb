f045e5d7aac890c1706b7eb42c1fa45a53b32d39
migue.masx@gmail.com <migue.masx@gmail.com>
2015-06-17 02:03:26 +0200

    gem acts_as_shopping_cart add models and migration cartitem

--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150616023053) do
+ActiveRecord::Schema.define(version: 20150616234014) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "articles", force: :cascade do |t|
-    t.string   "title"
-    t.string   "image_url"
-    t.string   "image_name"
+  create_table "cart_items", force: :cascade do |t|
+    t.integer  "owner_id"
+    t.string   "owner_type"
+    t.integer  "quantity"
+    t.integer  "item_id"
+    t.string   "item_type"
+    t.float    "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "carts", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end

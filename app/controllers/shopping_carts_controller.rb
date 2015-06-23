36a389cbb2db04654ae144abe69939f70abf3875
migue.masx@gmail.com <migue.masx@gmail.com>
2015-06-23 02:54:21 +0200

    shoping cart ajax

---
+++ app/controllers/shopping_carts_controller.rb
@@ -0,0 +1,26 @@
+class ShoppingCartsController < ApplicationController
+  before_filter :extract_shopping_cart
+
+  def create
+    # binding.pry
+    @product = Product.find(params[:product_id])
+    @shopping_cart.add(@product, @product.price)
+    respond_to do |format|
+      format.html { redirect_to @product, notice: 'User was successfully created.' }
+      format.js   { } 
+      format.json { render json: @product, status: :created, location: @product }
+    end
+  end
+
+  def show
+
+  end
+
+  private
+  def extract_shopping_cart
+    binding.pry
+    shopping_cart_id = session[:shopping_cart_id]
+    @shopping_cart = session[:shopping_cart_id] ? ShoppingCart.find(shopping_cart_id) : ShoppingCart.create
+    session[:shopping_cart_id] = @shopping_cart.id
+  end
+end
